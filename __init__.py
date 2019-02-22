4826f5c882877ecb21066c3c00c52fa46c9d6926
Donald Hutchison <git@toastymofo.net>
2019-02-22 17:20:51 +0100

    lights workin.

--- __init__.py
+++ __init__.py
@@ -4,21 +4,17 @@ import urequests as requests
 from random import randint, choice
 
 
-def disco_mode(pressed):
-    if pressed:
-        delay_for = randint(0, 600)
-        for i in range(delay_for):
-            data = []
-            for i in range(6):
-                G = randint(0, 255)
-                R = randint(0, 255)
-                B = randint(0, 255)
-                W = 0
-                data += [G,R,B,W]
-            badge.leds_send_data(bytes(data))
-            time.sleep(0.1)
-        data = [0] * 6 * 4
-        badge.leds_send_data(bytes(data))
+leds = [0] * 24
+idle = [5,6,7,8]
+
+def send_random_colors():
+    for i in range(6):
+        leds[i*4 ] = randint(0, 10) #G
+        leds[i*4 + 1] = randint(0, 10) #R
+        leds[i*4 + 2] = randint(0, 10) #B
+        leds[i*4 + 3] = 0 #W
+
+    badge.leds_send_data(bytes(leds), len(leds))
 
 def clear():
     ugfx.set_lut(ugfx.LUT_FULL)
@@ -28,28 +24,29 @@ def clear():
     ugfx.flush()
 
 def loop():
-    print("ey holmes")
 
+    while True:
+        if badge.eink_busy():
+            send_random_colors()
+        else:
+            ugfx.display_image(0,0, '/lib/dancin_disco_dave/trimmed_image_{}.png'.format(choice(idle)))
+            ugfx.flush()
+
+        #slows everything down and also allows processing input
+        time.sleep(0.1)
 
 def app_main():
     ugfx.init()
 
     clear()
     ugfx.set_lut(ugfx.LUT_FASTEST)
-    idle = [5,6,7,8]
 
     badge.vibrator_init()
 
-
-    ugfx.input_attach(ugfx.JOY_DOWN, lambda pressed: disco_mode(pressed))
     ugfx.input_attach(ugfx.BTN_A, lambda pressed: badge.vibrator_activate(0xd))
     ugfx.input_attach(ugfx.BTN_START, lambda pressed: appglue.home())
 
-    while True:
-        badge.eink_busy_wait()
-
-        ugfx.display_image(0,0, '/lib/dancin_disco_dave/trimmed_image_{}.png'.format(choice(idle)))
-        ugfx.flush()
+    loop()
 
 
 app_main()
